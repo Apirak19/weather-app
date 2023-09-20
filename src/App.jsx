8fd144b24ce44bca7a2e25ec32dbd37201b3af77
Apirak <apirakfakin@gmail.com>
2023-09-20 10:13:52 +0700

    api adjusted

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,26 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import anIcon from "./assets/react.svg";
 import "./App.css";
 
 function App() {
-  const [city, setCity] = useState("");
+  const [city, setCity] = useState("bangkok");
+  const [cityName, setCityName] = useState("");
+  const [temp, setTemp] = useState("");
   console.log(city);
   const getWeather = async () => {
-    let APIkey = "216a9c12e5c12c5e332dd7ddf2ab25b9";
-
-    if (city === "") {
-      return 0;
-    }
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=216a9c12e5c12c5e332dd7ddf2ab25b9`;
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=216a9c12e5c12c5e332dd7ddf2ab25b9`;
     let response = await fetch(url);
-    let data = await response.json()
-    
+    let data = await response.json();
+    if (city === "") {
+      setCityName(data.name);
+      setTemp(Math.floor(data.main.temp));
+    }
+    setCityName(data.name);
+    setTemp(Math.floor(data.main.temp));
   };
+  useEffect(() => {
+    getWeather();
+  }, []);
   return (
     <div className="container">
       <div className="search">
@@ -23,7 +28,9 @@ function App() {
           type="text"
           className="search-city"
           placeholder="a city eg. Bangkok"
+          value={city}
           onChange={(e) => setCity(e.target.value)}
+          onKeyDown={(e) => e.key === "Enter" && getWeather()}
         />
         <div
           className="search-icon"
@@ -37,13 +44,13 @@ function App() {
       <div className="weather-icon">
         <img src={anIcon} alt="" />
       </div>
-      <div className="temp">24</div>
-      <div className="city">Bangkok</div>
+      <div className="temp">{temp} °C</div>
+      <div className="cityName">{cityName}</div>
       <div className="data-container">
         <div className="data">
           <img src={anIcon} alt="" />
           <div className="detail">
-            <div className="text">Humidity</div>
+            <div className="text">Precipitation</div>
             <div className="number">90</div>
           </div>
         </div>
